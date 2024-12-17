15f2a873586a68af3e34813425dc87116e6906f6
tal erlich <tal.erlich@post.runi.ac.il>
2024-12-17 21:32:02 +0200

    Commenttt

--- Runigram.java
+++ Runigram.java
@@ -108,7 +108,8 @@ public class Runigram {
 	// lum = 0.299 * r + 0.587 * g + 0.114 * b, and returns a Color object consisting
 	// the three values r = lum, g = lum, b = lum.
 	private static Color luminance(Color pixel) {
-		Color lumi = new Color((int)(pixel.getRed()*0.299),(int)(pixel.getGreen()*0.587),(int)(pixel.getBlue()*0.114));
+		int lum = (int)(0.299 * pixel.getRed() + 0.587 * pixel.getGreen() + 0.114 * pixel.getBlue());
+		Color lumi = new Color(lum, lum, lum);
 		return lumi;
 	}
 	
